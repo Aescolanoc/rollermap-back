3cdfda9101ec3ec292c055fab7849946f9ff0255
Mel Escolano <amalia.escolano@gmail.com>
2022-03-14 17:50:46 +0100

    Created models

---
+++ models/rollerplace.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+export const rollerplaceSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  desciption: { type: String, required: true },
+  location: { type: Array, required: true },
+  type: { type: String, required: true },
+  slalom: { type: Boolean, required: true },
+  city: { type: String, required: true },
+  image: { type: String, required: true },
+  level: { type: String, required: true },
+  author: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+export const RollerPlace = mongoose.model('RollerPlace', rollerplaceSchema);
