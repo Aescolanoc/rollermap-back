c0e7194830115a59351079dd6957d4c165c1b515
Mel Escolano <amalia.escolano@gmail.com>
2022-03-15 20:53:54 +0100

    Added middleware loginrequired to routes

--- routes/rollerplaces.routes.js
+++ routes/rollerplaces.routes.js
@@ -8,14 +8,15 @@ import {
   insertRollerPlace,
   addToFavorites,
 } from '../controllers/rollerplace.controller.js';
+import { isAuthor, loginRequired } from '../middlewares/interceptors.js';
 const router = express.Router();
 
-router.get('/', getAllRollerPlaces);
-router.post('/', insertRollerPlace);
-router.get('/myrollerplaces', getMyRollerPlaces);
-router.get('/:id', getRollerPlaceDetails);
-router.patch('/:id', addToFavorites);
-router.put('/:id', updateRollerPlace);
-router.delete('/:id', deleteRollerPlace);
+router.get('/', loginRequired, getAllRollerPlaces);
+router.post('/', loginRequired, insertRollerPlace);
+router.get('/myrollerplaces', loginRequired, getMyRollerPlaces);
+router.get('/:id', loginRequired, getRollerPlaceDetails);
+router.patch('/:id', loginRequired, addToFavorites);
+router.put('/:id', loginRequired, isAuthor, updateRollerPlace);
+router.delete('/:id', loginRequired, deleteRollerPlace);
 
 export default router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import { insertUser, updateUser } from '../controllers/user.controller.js';
+import { loginRequired } from '../middlewares/interceptors.js';
 
 router.post('/', insertUser);
-router.patch('/:id', updateUser);
+router.patch('/:id', loginRequired, updateUser);
 
 export default router;
