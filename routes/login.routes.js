c75dc1fb2fc03bf04442bba57d502b7d110d9c9c
Mel Escolano <amalia.escolano@gmail.com>
2022-03-16 14:20:03 +0100

    Added middlewares on routes

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
 import { login } from '../controllers/login.controller.js';
+import { loginRequired } from '../middlewares/interceptors.js';
 
-router.post('/', login);
+router.post('/', loginRequired, login);
 
 export default router;
--- routes/rollerplaces.routes.js
+++ routes/rollerplaces.routes.js
@@ -6,17 +6,17 @@ import {
   updateRollerPlace,
   deleteRollerPlace,
   insertRollerPlace,
-  addToFavorites,
+  toggleFavorites,
 } from '../controllers/rollerplace.controller.js';
-import { loginRequired } from '../middlewares/interceptors.js';
+import { isAuthor, loginRequired } from '../middlewares/interceptors.js';
 const router = express.Router();
 
 router.get('/', loginRequired, getAllRollerPlaces);
 router.post('/', loginRequired, insertRollerPlace);
 router.get('/myrollerplaces', loginRequired, getMyRollerPlaces);
 router.get('/:id', loginRequired, getRollerPlaceDetails);
-router.patch('/:id', loginRequired, addToFavorites);
-router.put('/:id', loginRequired, updateRollerPlace);
-router.delete('/:id', loginRequired, deleteRollerPlace);
+router.patch('/:id', loginRequired, toggleFavorites);
+router.put('/:id', loginRequired, isAuthor, updateRollerPlace);
+router.delete('/:id', loginRequired, isAuthor, deleteRollerPlace);
 
 export default router;
