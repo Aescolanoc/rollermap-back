8426ad8567861189990642d8764bd33f697310fb
Mel Escolano <amalia.escolano@gmail.com>
2022-03-16 14:19:24 +0100

    Rollerplace controller control of errors

--- controllers/rollerplace.controller.js
+++ controllers/rollerplace.controller.js
@@ -4,7 +4,7 @@ import { User } from '../models/user.model.js';
 export const getAllRollerPlaces = async (req, res, next) => {
   try {
     const resp = await RollerPlace.find({});
-    res.json(resp);
+    res.status(200).json(resp);
   } catch (error) {
     next(error);
   }
@@ -12,8 +12,8 @@ export const getAllRollerPlaces = async (req, res, next) => {
 
 export const getMyRollerPlaces = async (req, res, next) => {
   try {
-    const resp = await RollerPlace.find({ author: req.body.id });
-    res.json(resp);
+    const resp = await RollerPlace.find({ author: req.tokenPayload.id });
+    res.status(200).json(resp);
   } catch (error) {
     next(error);
   }
@@ -24,11 +24,11 @@ export const insertRollerPlace = async (req, res, next) => {
     const result = RollerPlace.create(req.body);
 
     await User.findOneAndUpdate(
-      { _id: req.body.author },
+      { _id: req.tokenPayload.id },
       { $push: { myrollerplaces: result._id } }
     );
 
-    res.json(result);
+    res.status(201).json(result);
   } catch (error) {
     next(error);
   }
@@ -37,7 +37,7 @@ export const insertRollerPlace = async (req, res, next) => {
 export const getRollerPlaceDetails = async (req, res, next) => {
   try {
     const resp = await RollerPlace.findOne({ _id: req.params.id });
-    res.json(resp);
+    res.status(200).json(resp);
   } catch (error) {
     next(error);
   }
@@ -49,7 +49,7 @@ export const updateRollerPlace = async (req, res, next) => {
       { _id: req.params.id },
       req.body
     );
-    res.json(resp);
+    res.status(200).json(resp);
   } catch (error) {
     next(error);
   }
@@ -58,19 +58,38 @@ export const updateRollerPlace = async (req, res, next) => {
 export const deleteRollerPlace = async (req, res, next) => {
   try {
     const resp = await RollerPlace.findByIdAndDelete({ _id: req.params.id });
-    res.json(resp);
+    res.status(204).json(resp);
   } catch (error) {
     next(error);
   }
 };
 
-export const addToFavorites = async (req, res, next) => {
+export const toggleFavorites = async (req, res, next) => {
   try {
-    const resp = await User.findByIdAndUpdate(
-      { _id: req.body.id },
-      { $addToSet: { favorites: req.params.id } }
-    );
-    res.json(resp);
+    let foundUser = await User.findById({ _id: req.tokenPayload.id });
+    const processedFavorites = foundUser.favorites.map((e) => e.toString());
+    const isInFavorites = processedFavorites.some((e) => e === req.params.id);
+    let updatedUser;
+
+    if (isInFavorites) {
+      updatedUser = await User.findByIdAndUpdate(
+        req.tokenPayload.id,
+        {
+          $pull: { favorites: req.params.id },
+        },
+        { new: true }
+      );
+    } else {
+      updatedUser = await User.findByIdAndUpdate(
+        req.tokenPayload.id,
+        {
+          $addToSet: { favorites: req.params.id },
+        },
+        { new: true }
+      );
+    }
+
+    res.status(200).json(updatedUser);
   } catch (error) {
     next(error);
   }
